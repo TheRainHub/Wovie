8324e524aa73afc36afebbc3d93b8e489886f021
TheRainHub <theraintv94@gmail.com>
2024-10-05 21:54:22 +0200

    add registration and animations

--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const loginbuttonPopup = document.querySelector('.loginbutton-popup');
+const closeIcon = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
+});
+
+loginbuttonPopup.addEventListener('click', ()=>{
+    wrapper.classList.add('active-popup');
+});
+
+closeIcon.addEventListener('click', ()=>{
+    wrapper.classList.remove('active-popup');
+    wrapper.classList.remove('active');
+});
+
 document.querySelectorAll('.input-box input').forEach(input => {
     input.addEventListener('input', () => {
         if (input.value.trim() !== "") {
